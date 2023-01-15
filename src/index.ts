a3d01e5a93c0e09e1a96695f101668698b0ec7a9
Christopher Yu <christophergin88@gmail.com>
2023-01-15 19:38:28 +0800

    webhook call test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,8 +11,33 @@ app.use(
   })
 );
 
-app.post('/webhook', (req, res) => {
-  console.log(req.body);
+app.post('/webhook', async (req, res) => {
+  console.log(JSON.stringify(req.body));
+
+  if (req.body.events[0].type === 'message') {
+    await axios.post(
+      'https://api.line.me/v2/bot/message/reply',
+      {
+        replyToken: req.body.events[0].replyToken,
+        messages: [
+          {
+            type: 'text',
+            text: 'Yana dan gek esa',
+          },
+          {
+            type: 'text',
+            text: 'YANA GEK ESA',
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization:
+            'Bearer BNEwKTIQ2oA5UHrdir7mQV9GOwDVFG6LX0ReYTPDaqKx7M3IiE8WYq/c64yw2S8WRffv9/orAuEV9H3TkMEfOlMV3aHJA2yhTqzkZpoYPl6pJz9hTezYXQ2jUyMvDMak9z6xwgJm9LCVFENDFvy8+AdB04t89/1O/w1cDnyilFU=',
+        },
+      }
+    );
+  }
 
   return res.send('Post request to webhook');
 });
