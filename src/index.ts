70aaaa3fba36a1df58e923c5b20e0b8ae3caab2d
Christopher Yu <christophergin88@gmail.com>
2023-01-15 20:13:48 +0800

    bercanda

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,16 @@ app.post('/webhook', async (req, res) => {
   console.log(JSON.stringify(req.body));
 
   const events = req.body.events;
+  const randomAnswer = [
+    'Ya',
+    'Tidak',
+    'Tentu Iya',
+    'Tentu Tidak',
+    'Bisa Jadi',
+    'Mungkin',
+    'Coba Tanya Lagi',
+  ];
+  const randomNumber = Math.floor(Math.random() * randomAnswer.length);
 
   events.map(async (event: any) => {
     if (event.type === 'join') {
@@ -69,40 +79,26 @@ app.post('/webhook', async (req, res) => {
 
     if (event.type === 'message') {
       const text = event.message.text.toLowerCase();
+      if (text === 'apakah') {
+        await reply('Apakah apaan anjing, tanya yg bener', event.replyToken);
+      }
+
       if (text.includes('gustu') && text.includes('ganteng')) {
         await reply('Dan keren', event.replyToken);
       }
 
+      if (text.includes('yana') && text.includes('gek esa')) {
+        await reply(
+          'Yana dan Gek Esa sudah dipastikan untuk bersama di masa depan',
+          event.replyToken
+        );
+      }
+
       if (text.includes('apakah')) {
-        await reply('Ya', event.replyToken);
+        await reply(randomAnswer[randomNumber], event.replyToken);
       }
     }
   });
-
-  // if (req.body.events[0].type === 'message') {
-  //   await axios.post(
-  //     'https://api.line.me/v2/bot/message/reply',
-  //     {
-  //       replyToken: req.body.events[0].replyToken,
-  //       messages: [
-  //         {
-  //           type: 'text',
-  //           text: 'Yana dan gek esa',
-  //         },
-  //         {
-  //           type: 'text',
-  //           text: 'YANA GEK ESA',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       headers: {
-  //         Authorization:
-  //           'Bearer BNEwKTIQ2oA5UHrdir7mQV9GOwDVFG6LX0ReYTPDaqKx7M3IiE8WYq/c64yw2S8WRffv9/orAuEV9H3TkMEfOlMV3aHJA2yhTqzkZpoYPl6pJz9hTezYXQ2jUyMvDMak9z6xwgJm9LCVFENDFvy8+AdB04t89/1O/w1cDnyilFU=',
-  //       },
-  //     }
-  //   );
-  // }
 
   return res.send('Post request to webhook');
 });
